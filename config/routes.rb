2de69ddb6c257276797ad6a96c257fa434fde720
Clement des Brest <clement.desbrest@gmail.com>
2018-11-23 16:28:11 +0100

    Fix image upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :show, :new, :create] do
+  resources :cocktails, only: [:show, :new, :create] do
     resources :doses, only: [:new, :create]
     resources :reviews, only: [:new, :create, :edit, :update]
   end
   resources :doses, only: [:destroy]
   resources :reviews, only: [:destroy]
+  get '/', to: "cocktails#index", as: 'root'
 end
